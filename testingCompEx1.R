ccb72f28ceada324992fa90e41282020bb3d37a3
Jim Totland <jimtotland@gmail.com>
2021-02-10 15:03:20 +0100

    fixed my stupid misunderstanding of 1d) and added x0 in the calculation of value in 1e)

---
+++ testingCompEx1.R
@@ -0,0 +1,56 @@
+id <- "1X_8OKcoYbng1XvYFDirxjEWr7LtpNr1m" # google file ID
+values <- dget(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
+X = values$X
+dim(X)
+x0 = values$x0
+dim(x0)
+beta=values$beta
+dim(beta)
+sigma=values$sigma
+sigma
+
+library(ggplot2)
+bias = function(lambda,X,x0,beta)
+{
+  p = ncol(X)
+
+  #value = mean(t(x0) %*% solve((1+lambda)*diag(p)) %*% beta - t(x0) %*% beta )^2
+  #value = mean(t(x0) %*% solve(diag(p) + lambda*t(X) %*% X) %*% beta - t(x0) %*% beta )^2
+  #value = mean(t(x0) %*% solve(t(X) %*% X + lambda*diag(p)) %*% t(X) %*% X %*% beta - t(x0) %*%   #beta)^2
+
+  v1 <- t(x0) %*% solve(t(X) %*% X + lambda * diag(p)) %*% t(X) %*% X %*% beta - t(x0) %*% beta
+  v2 <- sigma^2 * t(x0) %*% (solve(t(X) %*% X + lambda * diag(p)) %*% t(X) %*% X %*% t(solve(t(X) %*% X + lambda * diag(p)))) %*% x0
+  v3 <- sigma^2
+  value <-  (t(x0) %*% solve(t(X) %*% X + lambda * diag(p)) %*% t(X) %*% X %*% beta - t(x0) %*% beta)^2
+  # Tror dette skal være rett algebraisk, men synes resultatet er merkelig kanskje!
+  # Kanskje oppgaven ovenfor burde fullføres først?
+  #value = mean(t(x0) %*% (diag(p) + 1/lambda*t(X)%*%X)%*%beta - t(x0) %*% beta)^2
+
+  # All of these give different plots!
+  return(value)
+}
+lambdas = seq(0, 2, length.out = 500)
+BIAS = rep(NA,length(lambdas))
+for (i in 1:length(lambdas)) BIAS[i] = bias(lambdas[i], X, x0, beta)
+dfBias = data.frame(lambdas = lambdas, bias = BIAS)
+ggplot(dfBias, aes(x = lambdas, y = bias)) +
+  geom_line(color = "red")+
+  xlab(expression(lambda))+
+  ylab(expression(bias^2))
+
+variance = function(lambda, X, x0, sigma)
+{
+  p = ncol(X)
+  inv = solve(t(X)%*%X+lambda*diag(p))
+  value = sigma*t(x0) %*% (inv%*%t(X)%*%X%*%t(inv)) %*% x0
+  return(value)
+}
+lambdas = seq(0, 2, length.out = 500)
+VAR=rep(NA,length(lambdas))
+for (i in 1:length(lambdas)) VAR[i]=variance(lambdas[i], X, x0, sigma)
+dfVar = data.frame(lambdas = lambdas, var = VAR)
+ggplot(dfVar, aes(x = lambdas, y = var))+
+  geom_line(color = "green4")+
+  xlab(expression(lambda))+
+  ylab("variance")
+
